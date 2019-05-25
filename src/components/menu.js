5f4fca3e1f2388a5a7bbf1767390033eb8ac5176
Sasha Miller <sashazm85@gmail.com>
2019-05-25 01:09:30 -0700

    added classNames to menu links

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,13 +1,15 @@
 import React from "react"
 import { Link } from "gatsby"
 
-const MenuMain = ({menuLinks}) => (
+const MenuMain = ({ menuLinks }) => (
   <ul className="menu menu--main menu--push">
-    {menuLinks.map(link=>
+    {menuLinks.map(link => (
       <li key={link.link}>
-        <Link to={link.link}>{link.name}</Link>
+        <Link to={link.link} className="menu__link">
+          {link.name}
+        </Link>
       </li>
-    )}
+    ))}
   </ul>
 )
 
