259b9437d2c02613a8bf4781d9702a3492a8ca19
Sasha Miller <sashazm85@gmail.com>
2019-05-25 17:58:10 -0700

    added a css class to style currently active link

--- src/components/menu.js
+++ src/components/menu.js
@@ -5,7 +5,11 @@ const MenuMain = ({ menuLinks }) => (
   <ul className="menu menu--main menu--push">
     {menuLinks.map(link => (
       <li key={link.link}>
-        <Link to={link.link} className="menu__link">
+        <Link
+          to={link.link}
+          className="menu__link"
+          activeClassName="menu--active"
+        >
           {link.name}
         </Link>
       </li>
